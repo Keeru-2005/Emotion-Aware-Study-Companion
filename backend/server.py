4b9da0a286e1a76dd7f87e6b6c60d8865658b32c
Bhoomika Basavaraj <bhoomikabasavaraj9@gmail.com>
2026-03-28 18:58:24 +0530

    Added project folders

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,15 @@
-from fastapi.middleware.cors import CORSMiddleware
+from emotion_detection.detect_emotion import detect_emotion_from_camera
 from fastapi import FastAPI
 from pydantic import BaseModel
-import openai
+from fastapi.middleware.cors import CORSMiddleware
+from google import genai
+import os
+import requests
+
+client = genai.Client(api_key="AIzaSyAWeb8aN5g91TSNBO6pC0eXHW-x4mj_Fz4")
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,25 +18,49 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-openai.api_key = "AIzaSyA3cbA12x6EfZC9VGuansAeeOPKT6_V154"
-
 class Question(BaseModel):
     question: str
 
 @app.post("/ask")
 def ask_ai(q: Question):
+    try:
+        response = client.models.generate_content(
+            model="gemini-1.5-flash",
+            contents=q.question
+        )
+        return {"answer": response.text}
+    except Exception as e:
+        return {"answer": str(e)}
 
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system",
-             "content": "You are a helpful study assistant. Explain concepts clearly for students."},
+@app.get("/detect_emotion")
+def detect_emotion():
 
-            {"role": "user",
-             "content": q.question}
-        ]
-    )
+    emotion = detect_emotion_from_camera()
 
-    answer = response["choices"][0]["message"]["content"]
+    if emotion in ["Sad", "Disgust"]:
+        state = "bored"
+    elif emotion in ["Fear", "Angry"]:
+        state = "stressed"
+    elif emotion == "Surprise":
+        state = "confused"
+    else:
+        state = "focused"
 
-    return {"answer": answer}
+    return {"emotion": state}
+
+YOUTUBE_API_KEY = os.getenv("AIzaSyDwmv-h2yFibyBWYtc8XXuXAHCIDCFOEBM")
+
+@app.get("/get_video")
+def get_video(topic: str):
+
+    try:
+        url = f"https://www.googleapis.com/youtube/v3/search?part=snippet&q={topic} lecture&type=video&key={YOUTUBE_API_KEY}"
+
+        res = requests.get(url).json()
+
+        video_id = res['items'][0]['id']['videoId']
+
+        return {"videoUrl": f"https://www.youtube.com/embed/{video_id}"}
+
+    except:
+        return {"videoUrl": "https://www.youtube.com/embed/dQw4w9WgXcQ"}
